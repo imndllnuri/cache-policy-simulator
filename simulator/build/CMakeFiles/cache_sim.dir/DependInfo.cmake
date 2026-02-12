e99172b033fbc0a4a620f40421660eec8b224dfb
imndllnuri <ni.imrendi@gmail.com>
2026-02-13 00:49:20 +0300

    CMake build, Clean repository, ignore build artifacts

---
+++ simulator/build/CMakeFiles/cache_sim.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nuri-imrendi/Projects/cache-policy-simulator/simulator/replacement/lru.cc" "CMakeFiles/cache_sim.dir/replacement/lru.cc.o" "gcc" "CMakeFiles/cache_sim.dir/replacement/lru.cc.o.d"
+  "/home/nuri-imrendi/Projects/cache-policy-simulator/simulator/src/cache.cc" "CMakeFiles/cache_sim.dir/src/cache.cc.o" "gcc" "CMakeFiles/cache_sim.dir/src/cache.cc.o.d"
+  "/home/nuri-imrendi/Projects/cache-policy-simulator/simulator/src/cache_set.cc" "CMakeFiles/cache_sim.dir/src/cache_set.cc.o" "gcc" "CMakeFiles/cache_sim.dir/src/cache_set.cc.o.d"
+  "/home/nuri-imrendi/Projects/cache-policy-simulator/simulator/src/main.cc" "CMakeFiles/cache_sim.dir/src/main.cc.o" "gcc" "CMakeFiles/cache_sim.dir/src/main.cc.o.d"
+  "/home/nuri-imrendi/Projects/cache-policy-simulator/simulator/src/policy_factory.cc" "CMakeFiles/cache_sim.dir/src/policy_factory.cc.o" "gcc" "CMakeFiles/cache_sim.dir/src/policy_factory.cc.o.d"
+  "/home/nuri-imrendi/Projects/cache-policy-simulator/simulator/src/trace_reader.cc" "CMakeFiles/cache_sim.dir/src/trace_reader.cc.o" "gcc" "CMakeFiles/cache_sim.dir/src/trace_reader.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simulator/build/CMakeFiles/cache_sim.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cache_sim.dir/replacement/lru.cc.o"
+  "CMakeFiles/cache_sim.dir/replacement/lru.cc.o.d"
+  "CMakeFiles/cache_sim.dir/src/cache.cc.o"
+  "CMakeFiles/cache_sim.dir/src/cache.cc.o.d"
+  "CMakeFiles/cache_sim.dir/src/cache_set.cc.o"
+  "CMakeFiles/cache_sim.dir/src/cache_set.cc.o.d"
+  "CMakeFiles/cache_sim.dir/src/main.cc.o"
+  "CMakeFiles/cache_sim.dir/src/main.cc.o.d"
+  "CMakeFiles/cache_sim.dir/src/policy_factory.cc.o"
+  "CMakeFiles/cache_sim.dir/src/policy_factory.cc.o.d"
+  "CMakeFiles/cache_sim.dir/src/trace_reader.cc.o"
+  "CMakeFiles/cache_sim.dir/src/trace_reader.cc.o.d"
+  "cache_sim"
+  "cache_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cache_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
